a9b91bec00de37a8d5cd7c0e8d80b31dcb75f1ce
critically-thinking <45378424+critically-thinking@users.noreply.github.com>
2019-06-20 22:04:02 -0500

    Add files via upload

---
+++ text_analyzer.py
@@ -0,0 +1,35 @@
+# Text Analyzer
+
+
+"""
+Write a program that determines how many times the following words
+appear with a text:
+
+Python
+Error
+"""
+
+def analyze_text(filename, *args):
+    """ Reads text and returns number of times the word is found in the text"""
+
+    with open(filename) as f_object: # open file in read-only mode
+        file_contents = f_object.read() # read entire text file
+
+    for word in args:
+        substring = word
+        if substring in file_contents:
+            word_count = file_contents.count(substring)
+            print( "The word {} appears in this text {} times.".format(substring,word_count))
+        #elif word not in contents:
+
+filename = 'chapter10.txt'
+result = analyze_text(filename, 'error', 'Python', 'the', 'and')
+        
+
+
+
+
+
+
+        
+    
